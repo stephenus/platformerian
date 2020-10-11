733fbd66bb14bd6522d5e74e423323da60965344
stephenus <stephenus@users.noreply.github.com>
2020-10-12 00:50:21 +0300

    Initial commit

---
+++ src/actors/Actor.gd
@@ -0,0 +1,12 @@
+extends KinematicBody2D
+class_name Actor
+
+export var speed: = Vector2(400.0, 500.0)
+export var gravity: = 2000.0
+
+const FLOOR_NORMAL: = Vector2.UP
+var _velocity: = Vector2.ZERO
+
+
+func _physics_process(delta: float) -> void:
+	_velocity.y += gravity * delta
---
+++ src/objects/Coin.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+
+onready var anim_player: AnimationPlayer = $AnimationPlayer
+
+export var score: = 1
+
+
+func _on_body_entered(body: PhysicsBody2D) -> void:
+	picked()
+
+
+
+# func picked() -> void:
+	# PlayerData.score += score
+	# anim_player.play("picked")
+
+
+func picked() -> void:
+	PlayerData.score += score
+	
